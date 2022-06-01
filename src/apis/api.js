8a68e036f33d30807626f69cd9bd0fed806bdfbd
thanhnth-0124 <nguyen.thi.hai.thanh@sun-asterisk.com>
2022-06-02 00:59:29 +0700

    change url

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { LOCAL_API_URL } from '../config';
+import { HEROKU_API_URL } from '../config';
 
 const axiosClient = axios.create({
-  baseURL: `${LOCAL_API_URL}`,
+  baseURL: `${HEROKU_API_URL}`,
   responseType: 'json',
   timeout: 15 * 1000,
   withCredentials: true
