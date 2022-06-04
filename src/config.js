c297a43b4c0aaf94c88440ec991a6785b0336071
thanhnth-0124 <nguyen.thi.hai.thanh@sun-asterisk.com>
2022-06-04 22:35:45 +0700

    Add config dev env

--- src/config.js
+++ src/config.js
@@ -6,3 +6,5 @@ export const COMMON_DATA_API = "/api/common-data";
 export const CLASS_API = "/api/classes";
 export const CV_API = "/api/cv";
 export const QUESTION_API = "/api/questions";
+
+export const MANAGER_APP_URL = process.env.NODE_ENV === 'heroku' ? process.env.REACT_APP_HEROKU_MANAGER_PAGE_URL : REACT_APP_LOCAL_MANAGER_PAGE_URL
